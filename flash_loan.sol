509343f2719c80876cba500166685f631cc3eb57
saleemoxd <98914547+saleemoxd@users.noreply.github.com>
2022-12-26 05:14:52 -0500

    Update flash_loan.sol

--- flash_loan.sol
+++ flash_loan.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.6;
+pragma solidity ^0.8.0;
 
 import "https://github.com/aave/aave-protocol-contracts/contracts/lending/FlashLoan.sol";
 import "https://github.com/KyberNetwork/smart-contracts/contracts/KyberNetwork.sol";
